d33391f9ed6fa1136267bbda109e2d70eb482d03
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-03-11 12:32:21 -0400

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,20 @@ using namespace std;
 
 int main(){
 	string name;
-	cout << "Hello, World!" << endl;
+	string bYear;
+	string favColor;
+	
+	cout << "Hello! Welcome to GitHub" << endl;
+	cout << "Please enter your name: " << endl;
 	cin >> name;
+	cout << "Please enter your birth year: " << endl;
+	cin >> bYear;
+	cout << "Please enter your favorite color: " << endl;
+	cin >> favColor;
+
+	cout << "Thank you! Loading..." << endl;
+
 	cout << "Hello, " << name << "!" << endl;
+	cout << "Bio" << endl << "-----------"<< endl << "Name: " << name << endl << "Birth Year: " << bYear << endl << "Favorite Color: " << favColor << endl;  
 	return 0;
 }
